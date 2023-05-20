cdb6fc5844f4615c7a1fbb2dd4c39fb61c0e631f
Daniel Yohannes <danielyohannes38@gmail.com>
2023-05-20 05:21:26 -0700

    Simple subtraction code that passes betty

---
+++ simple_program.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* Sub - subtracts two intiger numbers
+* @number1: The first intiger to be added
+* @number2: The second integer to be added
+*
+* Return: Returns difference of the two intigers
+*/
+
+int Sub(int number1, int number2)
+{
+	return (number1 - number2);
+}
